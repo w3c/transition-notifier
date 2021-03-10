720a2c9d73399b8bd95628740983452f3cee954f
plehegar <plh@w3.org>
2021-03-10 16:04:45 -0500

    Only notify on FPWD, not all WDs

--- notify.js
+++ notify.js
@@ -7,7 +7,7 @@ function notify(spec) {
   monitor.log("New document: " + spec.uri);
 
   if (spec.status === "Working Draft" || spec.status === "Candidate Recommendation") {
-    if (spec.obsoletes === undefined || spec.sotd.indexOf("wide review") !== -1) {
+    if (spec._links["predecessor-version"] === undefined || spec.sotd.indexOf("wide review") !== -1) {
       notifyWideReview(spec);
     } // else ignore
   } else if (spec.status === "Proposed Recommendation"
